91e568e2d915a21bd8177fd97a4ac90b2ef54594
tananaa <tanyaknz07@gmail.com>
2026-06-08 23:20:25 +0300

    remove _build from repo

--- _build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tanya/tananaa/workspace/lab05
-# Build directory: /home/tanya/tananaa/workspace/lab05/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/tanya/tananaa/workspace/lab05/_build/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/tanya/tananaa/workspace/lab05/CMakeLists.txt;21;add_test;/home/tanya/tananaa/workspace/lab05/CMakeLists.txt;0;")
-subdirs("banking")
